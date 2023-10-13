2d538c71fefc398d7bc32b2dc71b9fe924a3fa36
Shady Fanous <shadster09@gmail.com>
2023-10-13 16:26:19 +0300

    wip: adjustments and changes

--- src/app.ts
+++ src/app.ts
@@ -7,8 +7,10 @@ import * as actualAPI from "@actual-app/api";
 import express from "express";
 
 import Ajv, {JSONSchemaType} from "ajv"
+import addFormats from "ajv-formats"
 
 const ajv = new Ajv()
+addFormats(ajv, ["date", "uuid"])
 const app = express()
 const actualBudgetDataDir = './budget-data'
 
@@ -41,34 +43,66 @@ const env = envalid.cleanEnv(process.env, {
 
     // This is the ID from Settings → Show advanced settings → Sync ID
     await actualAPI.downloadBudget(env.ACTUAL_SERVER_BUDGET_ID);
-    const budget = await actualAPI.getAccounts();
-    console.log(budget);
-    await actualAPI.shutdown();
+    // await actualAPI.importTransactions("62fc2ba4-f047-4d5d-98f9-7dc088481b5a", [{
+    //     date: "2023-10-10",
+    //     amount: actualAPI.utils.amountToInteger(500.2),
+    //     notes: "This is a note",
+    //     payee_name: "Adidas"
+    // }])
+    // await actualAPI.shutdown();
 })();
 
-interface MyData {
-    foo: number
-    bar?: string
+interface TransactionData {
+    api_key: string
+    account: string
+    date: string
+    amount: number
+    payee_name: string
+    currency?: string
+    notes?: string
 }
 
 // Compile AJV
-const schema: JSONSchemaType<MyData> = {
+const schema: JSONSchemaType<TransactionData> = {
     type: "object",
     properties: {
-        foo: {type: "integer"},
-        bar: {type: "string", nullable: true}
+        api_key: {type: "string"},
+        account: {type: "string", format: "uuid"},
+        date: {type: "string", format: "date"},
+        amount: {type: "number"},
+        payee_name: {type: "string"},
+        currency: {type: "string", nullable: true, minLength: 3, maxLength: 3},
+        notes: {type: "string", nullable: true}
     },
-    required: ["foo"],
+    required: ["account", "date", "amount", "payee_name"],
     additionalProperties: false
 }
+
 const validate = ajv.compile(schema)
 
 app.use(express.json());
-app.post('/', async (req, res) => {
-    // Check API key from request
-    validate(req.body)
-    console.log(req)
-    res.status(201)
+app.post('/transactions', async (req, res) => {
+    if (!validate(req.body)) {
+        return res.status(403).send({"errors": validate.errors})
+    }
+    if (env.API_KEY !== req.body.api_key) {
+        return res.send(401)
+    }
+
+    // Convert to EGP if foreign currency
+    // Use API found here
+    // Have in a try / catch to avoid issues
+
+    // Run actual import of transaction
+    const payload = req.body
+    await actualAPI.importTransactions(payload.account, [{
+        date: payload.date,
+        payee_name: payload.payee_name,
+        amount: actualAPI.utils.amountToInteger(payload.amount),
+        notes: payload.notes
+    }])
+
+    return res.send(201)
 })
 
 // Setting up the server
