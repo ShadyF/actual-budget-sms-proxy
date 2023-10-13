ee1350937d767bb49e8bf5473a9d91997c451741
Shady Fanous <shadster09@gmail.com>
2023-10-13 21:57:55 +0300

    wip: move sms parser into its own module

--- src/app.ts
+++ src/app.ts
@@ -8,6 +8,7 @@ import express from "express";
 
 import Ajv, {JSONSchemaType} from "ajv"
 import addFormats from "ajv-formats"
+import {parseSMS} from "./parser";
 
 const ajv = new Ajv()
 addFormats(ajv, ["date", "uuid"])
@@ -52,33 +53,26 @@ const env = envalid.cleanEnv(process.env, {
     // await actualAPI.shutdown();
 })();
 
-interface TransactionData {
+interface RequestPayload {
     api_key: string
-    account: string
-    date: string
-    amount: number
-    payee_name: string
-    currency?: string
-    notes?: string
+    sms_sender: string
+    sms: string
 }
 
 // Compile AJV
-const schema: JSONSchemaType<TransactionData> = {
+const schema: JSONSchemaType<RequestPayload> = {
     type: "object",
     properties: {
         api_key: {type: "string"},
-        account: {type: "string", format: "uuid"},
-        date: {type: "string", format: "date"},
-        amount: {type: "number"},
-        payee_name: {type: "string"},
-        currency: {type: "string", nullable: true, minLength: 3, maxLength: 3},
-        notes: {type: "string", nullable: true}
+        sms_sender: {type: "string"},
+        sms: {type: "string"}
     },
-    required: ["account", "date", "amount", "payee_name"],
+    required: ["api_key", "sms_sender", "sms"],
     additionalProperties: false
 }
 
 const validate = ajv.compile(schema)
+
 
 app.use(express.json());
 app.post('/transactions', async (req, res) => {
@@ -89,17 +83,25 @@ app.post('/transactions', async (req, res) => {
         return res.send(401)
     }
 
+
+    // Attempt to parse SMS
+    let transactionData = null
+    try {
+        transactionData = parseSMS(req.body.sms_sender, req.body.sms)
+    } catch (e) {
+        return res.send(401)
+    }
+
     // Convert to EGP if foreign currency
     // Use API found here
     // Have in a try / catch to avoid issues
 
     // Run actual import of transaction
-    const payload = req.body
-    await actualAPI.importTransactions(payload.account, [{
-        date: payload.date,
-        payee_name: payload.payee_name,
-        amount: actualAPI.utils.amountToInteger(payload.amount),
-        notes: payload.notes
+    await actualAPI.importTransactions(transactionData.account, [{
+        date: transactionData.date,
+        payee_name: transactionData.payee_name,
+        amount: actualAPI.utils.amountToInteger(transactionData.amount),
+        notes: transactionData.notes
     }])
 
     return res.send(201)
