08f4145de944e44e21131cbc1100745f86660a94
Shady Fanous <shadster09@gmail.com>
2023-10-17 00:19:22 +0300

    feat: Support date inference if a "date" regex group is provided instead of "day", "month" and "year"

--- src/parser.ts
+++ src/parser.ts
@@ -31,8 +31,17 @@ export const parseSMS = (senderName: string, body: string, config: Record<string
     let transaction: TransactionData | null = null
     for (const parser of senderTParsers) {
         const matches = body.match(new RegExp(parser.regex, "i"))
-        if (matches && matches.groups?.year && matches.groups?.day && matches.groups?.month && matches.groups?.amount) {
-            const date = `${parser.append_year_prefix ? "20" : ""}${matches.groups.year}-${matches.groups.month}-${matches.groups.day}`
+        if (matches && matches.groups?.amount) {
+            let date = ""
+
+            if (matches.groups?.year && matches.groups?.day && matches.groups?.month) {
+                date = `${parser.append_year_prefix ? "20" : ""}${matches.groups.year}-${matches.groups.month}-${matches.groups.day}`
+            } else if (matches.groups?.date) {
+                date = new Date(matches.groups?.date).toLocaleDateString('en-CA')
+            } else {
+                throw Error("Could not parse date. Make sure you define either a 'date' group or a 'year', 'month' and 'day' groups in your regular expression.")
+            }
+
             transaction = {
                 account: parser.account,
                 date: date,
